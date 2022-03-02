f450f25143ba2896fa29b3f614f3892d329adf0a
seoyeon-baek <w2032@e-mirim.hs.kr>
2022-03-02 23:33:19 +0900

    Add: main components
    emotion/styled, styled-component, recoil setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import "./App.css";
+import {MainPage} from "./components";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path=" /" />
+        <Route path=" /" element={<MainPage />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/components/main/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import * as S from "./style";
+
+const MainPage = () => {
+  return <S.MainWrapper></S.MainWrapper>;
+};
+
+export default MainPage;
