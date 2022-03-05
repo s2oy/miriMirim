c0866d6d58faa47ee32779d5a0b4c979baaaa1d1
seoyeon-baek <w2032@e-mirim.hs.kr>
2022-03-05 19:35:42 +0900

    Fix: App.tsx import 오류 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,8 @@
 import React from "react";
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import "./App.css";
-<<<<<<< Updated upstream
-import {MainPage} from "./components";
-=======
 import MainPage from "./components/main";
 import QnaPage from "./components/qna";
->>>>>>> Stashed changes
 
 function App() {
   return (
