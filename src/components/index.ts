151959c8d13e4cf80b6cf36acb6ba125049a5e7f
seoyeon-baek <w2032@e-mirim.hs.kr>
2022-03-07 13:41:41 +0900

    Add: websolution page router connect

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,4 @@
 export {default as MainPage} from "./main";
+export {default as QnaPage} from "./qna";
+export {default as DesignPage} from "./design";
+export {default as WebPage} from "./web";
---
+++ src/components/web/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import * as S from "./style";
+
+const WebPage = () => {
+  return <></>;
+};
+
+export default WebPage;
