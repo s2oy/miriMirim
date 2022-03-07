759ad51d51aba77e9a298702ea6898a99312c56a
seoyeon-baek <w2032@e-mirim.hs.kr>
2022-03-07 09:57:27 +0900

    Add: Qna component layout 완성

--- src/components/qna/QnaItem.tsx
+++ src/components/qna/QnaItem.tsx
@@ -2,7 +2,17 @@ import React from "react";
 import * as S from "./style";
 
 const QnaItem = () => {
-  return <></>;
+  return (
+    <S.QnaWrapper>
+      <S.QnaContainer>
+        <div id="content">
+          <span>
+            선택지를 선택하세요. <br /> 클릭하면 넘어가게하기
+          </span>
+        </div>
+      </S.QnaContainer>
+    </S.QnaWrapper>
+  );
 };
 
 export default QnaItem;
