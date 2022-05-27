daae6907b3ae3afbf13f0897508ef91e60a5f128
seoyeon-baek <w2032@e-mirim.hs.kr>
2022-05-27 14:56:10 +0900

    중간작업

--- src/components/qna/QnaItem.tsx
+++ src/components/qna/QnaItem.tsx
@@ -1,15 +1,30 @@
 import React from "react";
 import * as S from "./style";
 
+const list = [
+  {
+    key: 1,
+    content: "대충 내용인 것 같다.",
+  },
+  {
+    key: 2,
+    content: "이것도 내용인 것 같다.",
+  },
+  {
+    key: 3,
+    content: "아마 이것도 뭔가 질문이지 않을까?",
+  },
+  {
+    key: 4,
+    content: "음.. 하나의 질문이 될 것 같다",
+  },
+];
+
 const QnaItem = () => {
+  // const [qnaValue, setQnaValue] = useState<any>();
   return (
     <S.QnaWrapper>
-      <S.QnaContainer>
-        <div id="content">
-          선택지를 선택하세요. <br />
-          클릭하면 넘어가게하기
-        </div>
-      </S.QnaContainer>
+      <S.QnaContainer>{/* <div id="content">{content}</div> */}</S.QnaContainer>
     </S.QnaWrapper>
   );
 };
