2a1b1f293bc7624a25f26a282dec1b6c5c425a50
seoyeon-baek <w2032@e-mirim.hs.kr>
2022-05-29 14:53:27 +0900

    Fix: json으로 변경 중

--- src/components/qna/QnaItem.tsx
+++ src/components/qna/QnaItem.tsx
@@ -1,30 +1,13 @@
 import React from "react";
 import * as S from "./style";
+// @ts-ignore
+import qna from "../../db/QnaList";
 
-const list = [
-  {
-    key: 1,
-    content: "대충 내용인 것 같다.",
-  },
-  {
-    key: 2,
-    content: "이것도 내용인 것 같다.",
-  },
-  {
-    key: 3,
-    content: "아마 이것도 뭔가 질문이지 않을까?",
-  },
-  {
-    key: 4,
-    content: "음.. 하나의 질문이 될 것 같다",
-  },
-];
-
-const QnaItem = () => {
-  // const [qnaValue, setQnaValue] = useState<any>();
+const QnaItem = ({qna}: any) => {
+  console.log(qna);
   return (
     <S.QnaWrapper>
-      <S.QnaContainer>{/* <div id="content">{content}</div> */}</S.QnaContainer>
+      <S.QnaContainer>{qna}</S.QnaContainer>
     </S.QnaWrapper>
   );
 };
--- src/components/qna/style.ts
+++ src/components/qna/style.ts
@@ -32,3 +32,5 @@ export const QnaContainer = styled.div`
     }
   }
 `;
+
+export const QnaListItem = styled.span``;
