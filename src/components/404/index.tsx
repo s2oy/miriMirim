f1c46b21e8cd87aa16559e12e1af7d00c931d5d8
seoyeon-baek <w2032@e-mirim.hs.kr>
2022-06-09 13:29:07 +0900

    Add: 404page

---
+++ src/components/404/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import * as S from "./style";
+
+const NotFound = () => {
+  return (
+    <>
+      <S.MainWrapper>
+        <S.Imgwrapper>404 not found page입니다.</S.Imgwrapper>
+      </S.MainWrapper>
+    </>
+  );
+};
+
+export default NotFound;
